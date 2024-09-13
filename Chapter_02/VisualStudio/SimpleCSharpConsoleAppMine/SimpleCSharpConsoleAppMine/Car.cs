b08ee09225e24f2a82a881986b2de24576f9c291
Bogdan Ostashevsky <rrrst97@gmail.com>
2024-09-13 15:13:50 +0300

    SimpleCSharpConsoleAppMine

---
+++ Chapter_02/VisualStudio/SimpleCSharpConsoleAppMine/SimpleCSharpConsoleAppMine/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCSharpConsoleAppMine;
+public class Car
+{
+    public string PetName { get; set; }
+    public string Make { get; set; }
+}
---
+++ Chapter_02/VisualStudio/SimpleCSharpConsoleAppMine/SimpleCSharpConsoleAppMine/Make.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCSharpConsoleAppMine
+{
+    public class Make
+    {
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
